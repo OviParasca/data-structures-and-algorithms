2b5e31d1948b3fb9de14e67685f487c786cacf9f
Ovi Parasca <OviParasca@gmail.com>
2018-05-16 01:06:19 -0700

    cleaned up the index.js code

--- index.js
+++ index.js
@@ -3,7 +3,5 @@
 const ArrayShift = require('./lib/array_shift.js');
 
 let myArray = new ArrayShift();
-console.log(`new array: ${myArray.insertShiftArray([2,4,6,8], 5)}`);
-
-let myArray2 = new ArrayShift();
-console.log(`\nnew array 2: ${myArray2.insertShiftArray([4,8,15,23,42], 16)}`);
+console.log(`new array:   ${myArray.insertShiftArray([2, 4, 6, 8], 5)}`);
+console.log(`new array 2: ${myArray.insertShiftArray([4, 8, 15, 23, 42], 16)}`);
