074682a163504c937c7ff0617c255101f91a1aff
Ovi Parasca <OviParasca@gmail.com>
2018-05-20 18:31:57 -0700

    - fixed a typo in my index. js file

--- whiteboard-challenge-02/index.js
+++ whiteboard-challenge-02/index.js
@@ -6,4 +6,4 @@ let myArray = new ArrayShift();
 console.log(`new array 1: ${myArray.insertShiftArray([2, 4, 6, 8], 5)}`);
 console.log(`new array 2: ${myArray.insertShiftArray([4, 8, 15, 23, 42], 16)}`);
 
-console.log(`remove middle element from array: ${myArray.removeShiftArray([1, 2, 3, 4, 5])}`)
+console.log(`remove middle element from array: ${myArray.removeShiftArray([1, 2, 3, 4, 5])}`);
