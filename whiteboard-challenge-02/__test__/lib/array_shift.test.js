6d21638d91351e0d0f866915f135a575928bd0e8
Ovi Parasca <OviParasca@gmail.com>
2018-05-20 17:15:42 -0700

    - updated the white board #2 repo - updated the code and added more tests for the remove from middle of array function

--- whiteboard-challenge-02/__test__/lib/array_shift.test.js
+++ whiteboard-challenge-02/__test__/lib/array_shift.test.js
@@ -9,7 +9,11 @@ describe('ArrayShift', () => {
         return testArray.insertShiftArray([2, 4, 6, 8], 5);
     };
 
+    let loadRemoveFromArray = () => {
+        return testArray.removeShiftArray([1, 2, 3, 4, 5]);
+    }
 
+    // The following tests are for the inserting a new element in the middle of an array
     it('check the array for the valid length after inserting the new value', () => {
         expect(loadArray().length).toEqual(5);
     });
@@ -23,6 +27,21 @@ describe('ArrayShift', () => {
     it('check if the new value is in the center', () => {
         const centerPoint = Math.ceil([2, 4, 6, 8].length/2);
         expect(loadArray()[centerPoint]).toEqual(5);
+    });
+
+    // The following tests are for removing the middle element from an array
+    it('check the array for the valid length after removing the middle element', () => {
+        expect(loadRemoveFromArray().length.toEqual(4));
+    });
+
+    it('check if all the elements in the array are numbers', () => {
+        loadRemoveFromArray().forEach((val,i) => {
+            expect(typeof val).toEqual("number");
+        });
+    });
+
+    it('check if the middle element is removed', () => {
+        const centerPoint = Math.floor([2, 4, 6, 8].length/2);
     });
 
 });
