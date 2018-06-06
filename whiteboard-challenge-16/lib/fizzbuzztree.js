2f803b6d513d90a1be906cd990ef7013680fd9d1
Ovidiu Parasca <OviParasca@gmail.com>
2018-06-06 13:54:25 -0700

    - finished whiteboard 16

--- whiteboard-challenge-16/lib/fizzbuzztree.js
+++ whiteboard-challenge-16/lib/fizzbuzztree.js
@@ -8,12 +8,6 @@ class FizzBuzzTree {
   fizzbuzztree(BinaryTree) {
     let _walk = (node) => {
       node.value = node.value % 15 === 0 ? 'fizzbuzz' : node.value % 5 === 0 ? 'buzz' : node.value % 3 === 0 ? 'fizz' : node.value;
-      // myString = node.value % 3 === 0 ? myString += 'fizz' : '';
-      // myString = node.value % 5 === 0 ? myString += 'buzz' : myString;
-      // if (myString.length > 0) {
-      //   node.value = myString;
-      //  myString = '';
-      // }
       if (node.left) _walk(node.left);
       if (node.right) _walk(node.right);
     };
