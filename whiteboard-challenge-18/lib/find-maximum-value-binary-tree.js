4783773007ba840d13df6cd8d36ace682c573a6e
Ovidiu Parasca <OviParasca@gmail.com>
2018-06-06 21:20:37 -0700

    - finished the whiteboard 18 code

--- whiteboard-challenge-18/lib/find-maximum-value-binary-tree.js
+++ whiteboard-challenge-18/lib/find-maximum-value-binary-tree.js
@@ -1,2 +1,23 @@
 'use strict';
 
+const BinaryTree = require('../../binary-tree/lib/BinaryTree.js');
+const Node = require('../../binary-tree/lib/Node.js');
+
+class FindMaxValue {
+
+  findMaximumValue(tree) {
+    var maxValue = tree.root.value;
+
+    let _walk = (node) => {
+      maxValue = node.value > maxValue ? node.value : maxValue;
+      if(node.left) _walk(node.left);
+      if(node.right) _walk(node.right);
+    }
+    _walk(tree.root);
+
+    return maxValue;
+  }
+
+}
+
+module.exports = FindMaxValue;
