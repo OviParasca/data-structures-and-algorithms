7b59b4f12dcb401dc531db454468220b1aad1957
Ovidiu Parasca <OviParasca@gmail.com>
2018-06-06 20:48:42 -0700

    - finalized the Towers of hanoi whiteboard

--- whiteboard-challenge-14/__test__/lib/towers-of-hanoi.test.js
+++ whiteboard-challenge-14/__test__/lib/towers-of-hanoi.test.js
@@ -1,2 +1,25 @@
 'use strict';
 
+const Hanoi = require('../../lib/towers-of-hanoi.js');
+
+describe('Tests for Towers Of Hanoi', () => {
+
+  it ('Count the number of steps required to meet the results for 3 discs', () => {
+    let toh = new Hanoi;
+    toh.hanoi(3, 'start', 'buffer', 'dest');
+    expect(toh.steps).toBe(7);
+  });
+
+  it ('Count the number of steps required to meet the results for 2 discs', () => {
+    let toh = new Hanoi;
+    toh.hanoi(2, 'start', 'buffer', 'dest');
+    expect(toh.steps).toBe(3);
+  });
+
+  it ('Count the number of steps required to meet the results for 7 discs', () => {
+    let toh = new Hanoi;
+    toh.hanoi(7, 'start', 'buffer', 'dest');
+    expect(toh.steps).toBe(127);
+  });
+
+});
--- whiteboard-challenge-14/lib/towers-of-hanoi.js
+++ whiteboard-challenge-14/lib/towers-of-hanoi.js
@@ -1,2 +1,20 @@
 'use strict';
 
+class TowersOfHanoi {
+
+  constructor() {
+     this.steps = 0;
+  };
+
+  hanoi (disc, start, buffer, dest) {
+    if (disc > 0) {
+      this.hanoi(disc -1, start, dest, buffer);
+      console.log('Move disc ' + disc + ' from ' + start + ' to ' + dest);
+      this.steps++;
+      this.hanoi(disc -1, buffer, start, dest);
+    }
+  };
+
+}
+
+module.exports = TowersOfHanoi;
