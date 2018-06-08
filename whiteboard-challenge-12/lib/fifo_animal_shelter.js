547a5a914d71f574894a895a653bca1237ec17dd
Ovidiu Parasca <OviParasca@gmail.com>
2018-06-08 10:17:21 -0700

    - added tests for whiteboard lab 12

--- whiteboard-challenge-12/lib/fifo_animal_shelter.js
+++ whiteboard-challenge-12/lib/fifo_animal_shelter.js
@@ -1,1 +1,36 @@
 'use strict';
+
+class AnimalShelter {
+
+  constructor() {
+    this.dog = [];
+    this.cat = [];
+  }
+
+  enqueueAnimal(value) {
+    if (value === 'cat') {
+      this.cat[this.cat.length] = value;
+    } else if (value === 'dog') {
+      this.dog[this.dog.length] = value;
+    }
+  }
+
+  dequeueAnimal(pref) {
+    if (pref === 'dog') {
+      for (var i = 1; i < this.dog.length; i++) {
+        this.dog[i-1] = this.dog[i];
+      }
+      this.dog.length--;
+    }
+
+    if (pref === 'cat') {
+      for (var i = 1; i < this.cat.length; i++) {
+        this.cat[i-1] = this.cat[i];
+      }
+      this.cat.length--;
+    }
+  }
+
+}
+
+module.exports = AnimalShelter;
