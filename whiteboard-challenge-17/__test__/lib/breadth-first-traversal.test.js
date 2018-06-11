e67c638c2dc05e4e4078f6f1d53fe026218d2eb6
Ovidiu Parasca <OviParasca@gmail.com>
2018-06-10 18:18:55 -0700

    - added the main logic and unit tests

---
+++ whiteboard-challenge-17/__test__/lib/breadth-first-traversal.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const BinaryTree = require('../../../binary-tree/lib/BinaryTree.js');
+const Node = require('../../../binary-tree/lib/Node.js');
+const BreadthFirstTree = require('../../lib/breadth-first-traversal.js');
+
+describe('Tests for the Breadth First Traversal', () => {
+
+  const one = new Node(1);
+  const two = new Node(3);
+  const three = new Node(5);
+  const four = new Node(9);
+  const five = new Node(10);
+  const six = new Node(12);
+  const seven = new Node(15);
+  const eight = new Node(20);
+  const nine = new Node(30);
+
+  const tree = new BinaryTree(one);
+
+  one.left = two;
+  one.right = three;
+  three.left = four;
+  three.right = five;
+  two.left = six;
+  six.right = seven;
+  seven.left = eight;
+  seven.right = nine;
+
+  it('check if the root node is correct', () => {
+    expect(one.left.value).toBe(3);
+  });
+
+  it('check if the specific node value is correct', () => {
+    expect(one.right.value).toBe(5);
+  });
+
+  it('check if the specific node value is correct', () => {
+    expect(seven.right.value).toBe(30);
+  });
+
+});
--- whiteboard-challenge-17/index.js
+++ whiteboard-challenge-17/index.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const BinaryTree = require('../binary-tree/lib/BinaryTree.js');
+const Node = require('../binary-tree/lib/Node.js');
+const BreadthFirstTree = require('./lib/breadth-first-traversal.js');
+
+const one = new Node(1);
+const two = new Node(3);
+const three = new Node(5);
+const four = new Node(9);
+const five = new Node(10);
+const six = new Node(12);
+const seven = new Node(15);
+const eight = new Node(20);
+const nine = new Node(30);
+
+const tree = new BinaryTree(one);
+
+one.left = two;
+one.right = three;
+three.left = four;
+three.right = five;
+two.left = six;
+six.right = seven;
+seven.left = eight;
+seven.right = nine;
+
+
+let bst = new BreadthFirstTree();
+console.log(bst.breadthFirstTraversal(tree));
